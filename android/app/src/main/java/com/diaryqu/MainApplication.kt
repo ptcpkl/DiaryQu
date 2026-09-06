3a4918a235dac755c9404d563d58591bceb5f036
ptcpkl <ptcpkls.say.berk@gmail.com>
2026-09-06 22:31:09 +0700

    feat(agenda): register Android reminder package

--- android/app/src/main/java/com/diaryqu/MainApplication.kt
+++ android/app/src/main/java/com/diaryqu/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.diaryqu
 
 import android.app.Application
+import com.diaryqu.agenda.AgendaReminderPackage
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -14,8 +15,7 @@ class MainApplication : Application(), ReactApplication {
       context = applicationContext,
       packageList =
         PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
+          add(AgendaReminderPackage())
         },
     )
   }
